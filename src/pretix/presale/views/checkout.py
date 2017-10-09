c515f9177d1512252169649a88da290f7db083cd
Raphael Michel <mail@raphaelmichel.de>
2017-10-09 13:11:20 +0200

    More isolation steps

--- src/pretix/presale/views/checkout.py
+++ src/pretix/presale/views/checkout.py
@@ -10,15 +10,20 @@ from pretix.base.services.cart import CartError
 from pretix.base.signals import validate_cart
 from pretix.multidomain.urlreverse import eventreverse
 from pretix.presale.checkoutflow import get_checkout_flow
-from pretix.presale.views.cart import (
-    allow_frame_if_namespaced, get_or_create_cart_id,
-)
+from pretix.presale.views import allow_frame_if_namespaced
+from pretix.presale.views.cart import get_or_create_cart_id
 
 
 @method_decorator(allow_frame_if_namespaced, 'dispatch')
 class CheckoutView(View):
-    def dispatch(self, request, *args, **kwargs):
 
+    def get_index_url(self, request):
+        kwargs = {}
+        if request.resolver_match and 'cart_namespace' in request.resolver_match.kwargs:
+            kwargs['cart_namespace'] = request.resolver_match.kwargs['cart_namespace']
+        return eventreverse(self.request.event, 'presale:event.index', kwargs=kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
         self.request = request
         cart_pos = CartPosition.objects.filter(
             cart_id=get_or_create_cart_id(request), event=self.request.event
@@ -26,11 +31,11 @@ class CheckoutView(View):
 
         if not cart_pos.exists() and "async_id" not in request.GET:
             messages.error(request, _("Your cart is empty"))
-            return redirect(eventreverse(self.request.event, 'presale:event.index'))
+            return redirect(self.get_index_url(self.request))
 
         if not request.event.presale_is_running:
             messages.error(request, _("The presale for this event is over or has not yet started."))
-            return redirect(eventreverse(self.request.event, 'presale:event.index'))
+            return redirect(self.get_index_url(self.request))
 
         cart_error = None
         try:
@@ -45,14 +50,14 @@ class CheckoutView(View):
                 continue
             if step.requires_valid_cart and cart_error:
                 messages.error(request, str(cart_error))
-                return redirect(previous_step.get_step_url() if previous_step
-                                else eventreverse(self.request.event, 'presale:event.index'))
+                return redirect(previous_step.get_step_url(request) if previous_step
+                                else self.get_index_url(request))
 
             if 'step' not in kwargs:
-                return redirect(step.get_step_url())
+                return redirect(step.get_step_url(request))
             is_selected = (step.identifier == kwargs.get('step', ''))
             if "async_id" not in request.GET and not is_selected and not step.is_completed(request, warn=not is_selected):
-                return redirect(step.get_step_url())
+                return redirect(step.get_step_url(request))
             if is_selected:
                 if request.method.lower() in self.http_method_names:
                     handler = getattr(step, request.method.lower(), self.http_method_not_allowed)
